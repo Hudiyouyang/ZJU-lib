8b248fb7ccc098bfaadc5273814458224428dc01
Hudiyouyang <96237069+Hudiyouyang@users.noreply.github.com>
2022-10-22 23:03:21 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -3,32 +3,54 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from plyer import notification
 
-driverfile_path = r'D:\Program Files\Python39\msedgedriver.exe'
+seat_type = int(input("输入要预约的座位类型："))    #输入需要预约的座位类型
+                            # 1：二楼外文 2：三楼信息空间
+
+driverfile_path = r'D:\Program Files\Python39\msedgedriver.exe'     #在此填入引擎驱动的绝对路径
 driver = webdriver.Edge(executable_path=driverfile_path)
 
 driver.get(r'https://zjuam.zju.edu.cn/cas/login?service=http://ic.zju.edu.cn')
 
-driver.find_element('id','username').send_keys('xxxxxxx')           #在此输入学号
-driver.find_element('id','password').send_keys('xxxxxxx')          #在此输入浙大通行证密码
+driver.find_element('id','username').send_keys('xxxxxx')           #在此输入学号
+driver.find_element('id','password').send_keys('xxxxxx')          #在此输入浙大通行证密码
 
 driver.find_element('id','dl').click()
 
-sleep(10)
+sleep(5)
 
-seat_num = int(driver.find_element(By.XPATH,'/html/body/div/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/div[2]/div[5]/div[2]/div[1]/span').text)
+if seat_type == 2:
+    seat_num = int(driver.find_element(By.XPATH,
+                                       '/html/body/div/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/div[2]/div[5]/div[2]/div[1]/span').text)
 
-while seat_num == 0:
-    sleep(60)
-    driver.refresh()
-    sleep(10)
-    seat_num = int(driver.find_element(By.XPATH,'/html/body/div/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/div[2]/div[5]/div[2]/div[1]/span').text)
+    while seat_num == 0:
+        sleep(60)
+        driver.refresh()
+        sleep(10)
+        seat_num = int(driver.find_element(By.XPATH,
+                                           '/html/body/div/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/div[2]/div[5]/div[2]/div[1]/span').text)
 
+    notification.notify(
+        title='图书馆预约提醒',
+        message='三楼信息空间存在空位',
+        app_icon=None,
+        timeout=10,
+    )
 
-notification.notify(
-    title = '图书馆预约提醒',
-    message = '三楼信息空间存在空位',
-    app_icon = None,
-    timeout = 10,
-)
+if seat_type == 1:
+    seat_num = int(driver.find_element(By.XPATH,
+                                       '//*[@id="app"]/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/div[2]/div[4]/div[2]/div[1]/span').text)
 
+    while seat_num == 0:
+        sleep(60)
+        driver.refresh()
+        sleep(10)
+        seat_num = int(driver.find_element(By.XPATH,
+                                           '//*[@id="app"]/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/div[2]/div[4]/div[2]/div[1]/span').text)
+
+    notification.notify(
+        title='图书馆预约提醒',
+        message='二楼外文存在空位',
+        app_icon=None,
+        timeout=10,
+    )
 
